214c3c9c1305da120e306c3812cbb332947d961b
MakinPusing <90222170+makinpusing@users.noreply.github.com>
2023-06-12 02:38:28 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -16,7 +16,7 @@ global.ONLY_GROUPS = false // Only Groups Use
 /*
 🤖 𝗕𝗢𝗧 𝗜𝗡𝗙𝗢 𝗦𝗘𝗧𝗧𝗜𝗡𝗚𝗦
 */
-global.OWNER_NAME = 'Dark Maker Ofc' // Owner Name
+global.OWNER_NAME = 'Makin Pusing' // Owner Name
 global.OWNER_NUMBER = ['6281912414328'] //Owner Number
 global.BOT_ADMINS = ['94715166712','94715166712'] //Bot admins numbers
 global.BLOCK_CHAT = [] // If you want Block Bot From Chats Use .jid Command and put Jids
