11daa474f75b809e60eeccf524e59980685103ee
MakinPusing <90222170+makinpusing@users.noreply.github.com>
2023-06-12 17:05:00 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -33,8 +33,8 @@ global.AI_PASSWORD = 'king-nima-gpt-bot'
 /*
 🎲 𝗢𝗧𝗛𝗘𝗥 𝗦𝗘𝗧𝗜𝗡𝗚𝗦
 */
-global.CMD1 = 'aiold' // .ai Command
-global.CMD2 = 'ai' // .ai2 Command
+global.CMD1 = 'ai1' // .ai Command
+global.CMD2 = '' // .ai2 Command
 global.PORT = process.env.PORT || '5000' // Port No need Change.
 
 
